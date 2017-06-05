d60bc34410da95d5225e869f801f9e587aec71c7
marante <bugger@bravis.se>
2017-06-05 22:08:51 +0200

    Add project files.

---
+++ VidlyUdemyTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidlyUdemyTest.Startup))]
+namespace VidlyUdemyTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
