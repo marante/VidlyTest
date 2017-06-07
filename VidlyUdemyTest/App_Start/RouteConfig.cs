4b546499baac5e22b05b1e617bdcdf5c47306c81
marante <bugger@bravis.se>
2017-06-07 13:12:26 +0200

    Worked on the basics, reached first exercise

--- VidlyUdemyTest/App_Start/RouteConfig.cs
+++ VidlyUdemyTest/App_Start/RouteConfig.cs
@@ -13,10 +13,12 @@ namespace VidlyUdemyTest
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Movies", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ VidlyUdemyTest/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyUdemyTest.Models;
+using VidlyUdemyTest.ViewModel;
+
+namespace VidlyUdemyTest.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        // GET: Movies/Index
+        public ActionResult Index()
+        {
+
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Terminator 3" },
+                new Movie { Id = 2, Name = "Shrek" },
+                new Movie { Id = 3, Name = "Top Gun" },
+                new Movie { Id = 4, Name = "Lord of the Rings" },
+                new Movie { Id = 5, Name = "Star Wars" }
+            };
+
+            return View(movies);
+        }
+
+        // GET: Movies/Details/{id}
+        public ActionResult Details(int id)
+        {
+            
+        }
+
+
+    }
+}
